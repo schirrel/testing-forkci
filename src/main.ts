dad74eaa53946edf79a58547430e9217be411276
Alan Schio <6757777+schirrel@users.noreply.github.com>
2025-01-25 23:54:22 -0400

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+//Dummy file from https://www.typescriptlang.org/docs/handbook/2/basic-types.html
+// This is an industrial-grade general-purpose greeter function:
+function greet(person, date) {
+  console.log(`Hello ${person}, today is ${date}!`);
+}
+ 
+greet("Schirrel");
