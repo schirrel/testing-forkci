935bb7755c012096477524f543d4acf4de311fd3
Alan Schio <6757777+schirrel@users.noreply.github.com>
2025-01-26 00:29:48 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 //Dummy file from https://www.typescriptlang.org/docs/handbook/2/basic-types.html
 // This is an industrial-grade general-purpose greeter function:
-function greet(person, date) {
+function greet(person: string, date?: Date) {
   console.log(`Hello ${person}, today is ${date}!`);
 }
  
