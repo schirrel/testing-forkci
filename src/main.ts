f0b0db0c261e5ccbb3541280956b554b162066ce
Alan Schio <alanbschio@gmail.com>
2025-01-26 00:35:51 -0400

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 //Dummy file from https://www.typescriptlang.org/docs/handbook/2/basic-types.html
 // This is an industrial-grade general-purpose greeter function:
-function greet(person: string, date?: Date) {
-  console.log(`Hello ${person}, today is ${date}!`);
+function greet(person: string, date?: Date) {;
+  const _date = date ?? new Date()
+  console.log(`Hello ${person}, today is ${_date}!`);
 }
- 
+
 greet("Schirrel");
